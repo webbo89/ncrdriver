af4f2d078072ac7e7d3c3fc6cd798a6059fa945b
Max Revitt <max.revitt@gmail.com>
2012-11-27 18:52:53 +0000

    stable compiling

--- c++/main.cpp
+++ c++/main.cpp
@@ -1,68 +1,49 @@
 #include <iostream>
-#include <fstream>
-#include <boost/thread.hpp>
-#include "BufferedAsyncSerial.h"
 #include <string>
+#include <boost/thread.hpp>
+#include <sstream>
+#include "BufferedAsyncSerial.h"
+#include "../libqrencode/qrencode.h"
+
 
 using namespace std;
 using namespace boost;
 
 int main(int argc, char* argv[])
 {
-	string line;
-	ifstream::pos_type size;
-	char * memblock;
-	char * cmdblock;
-	string stringblock;
+        string stringblock;
+        unsigned char * data;
+        int width;
+        stringstream ss;
+        QRcode *qrcode;
+        
+        try {
+            BufferedAsyncSerial serial("/dev/ttyUSB0",9600);
+            
+            stringblock = ('\x1b');
+            
+            //memblock = QRcode_encodeString("http://www.google.co.uk", 0, QR_ECLEVEL_H, QR_MODE_8, 0)->data;
+            
+            qrcode = QRcode_encodeString("abcdefghijk123456789012", 0, QR_ECLEVEL_H, QR_MODE_8, 0);
 
-	try {
-        BufferedAsyncSerial serial("/dev/ttyUSB0",9600);
-		if(argc > 1) {
-			ifstream inputfile (argv[1], ios::in|ios::binary|ios::ate);
-			if (inputfile.is_open())
-			{
-				size = inputfile.tellg();
-				memblock = new char [size];
-				inputfile.seekg (0, ios::beg);
+            data = qrcode->data;
+            width = qrcode->width;
+            
+            cout << width;
+            QRcode_free(qrcode);
+            
+            ss << data;
+            
+            stringblock += ss.str();
+            
+            serial.writeString(stringblock);
+            
+            serial.writeString("\x1d\x2f\x0");
 
-				cout <<'\x1b';
-				//serial.writeString("\x1b"); // BMP to RAM pg164 of NCR7167
-				stringblock = "";
-
-				stringblock+=('\x1b');
-
-				inputfile.read (memblock, size);
-				inputfile.close();
-
-				for (int i = 0; i < size; i++) //  doesn't work with '\0' either
-				{
-					stringblock+=(memblock[i]);
-				}
-				
-				//serial.writeString(stringblock);
-				 
-				//cout << hex <<'\x1b' << memblock << "\n";
-			//	cout << "Just about to write memblock " << size << "\n";
-
-				//serial.writeString('\x1b'+stringblock);
-
-				//serial.writeString("\n");
-				
-				//cout << "Written memblock and vblf \n";
-			
-				//serial.writeString("\x1d\x2f\x0"); // print RAM image normal density pg171 of NCR7167
-
-				//cout << "Done" << "\n";
-
-				//delete[] memblock;
-			}
-		}
-		cout << "closing serial \n";
-		serial.close();
+            serial.close();
   
-    } catch(boost::system::system_error& e)
-    {
-        cout<<"Error: "<<e.what()<<endl;
-        return 1;
-    }
+        } catch(boost::system::system_error& e) {
+                cout<<"Error: "<<e.what()<<endl;
+                return 1;
+        }
 }
