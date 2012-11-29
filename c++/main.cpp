305155e4d0ff41a8a2ebcf409b8a17b56290ee29
Max Revitt <max.revitt@gmail.com>
2012-11-29 18:57:35 +0000

    ok working with working bitmaps lol

--- c++/main.cpp
+++ c++/main.cpp
@@ -16,7 +16,7 @@ int main(int argc, char* argv[])
 	string stringblock;
 
 	try {
-        BufferedAsyncSerial serial("/dev/ttyUSB0",9600);
+        //BufferedAsyncSerial serial("/dev/ttyUSB0",9600);
 		if(argc > 1) {
 			ifstream inputfile (argv[1], ios::in|ios::binary|ios::ate);
 			if (inputfile.is_open())
@@ -29,28 +29,30 @@ int main(int argc, char* argv[])
 				//serial.writeString("\x1b"); // BMP to RAM pg164 of NCR7167
 				stringblock = "";
 
-				//stringblock+=("\x1b");
-serial.writeString("\x1b");
+				//serial.writeString("\x1b");
 
 				inputfile.read (memblock, size);
 				inputfile.close();
 
 				for (int i = 0; i < size; i++) //  doesn't work with '\0' either
 				{
-					stringblock.append((char*)&memblock[i], 1);
-					//cout << stringblock.substr(i, 1) << endl;
+					stringblock += memblock[i];//append((char*)&memblock[i], 1);
+					if(memblock[i] != ' '){
+					//serial.write((char *)&memblock[i], 1);					
+					cout << memblock[i];
+					}
 				}
-				cout << stringblock << endl;
+				//cout << stringblock << endl;
 				
-				//serial.writeString(stringblock);
+				//serial.writeString('\x1b' + stringblock);
 
-				serial.write(stringblock.c_str(), (int)size);
+				//serial.write(stringblock.c_str(), (int)size);
 
-				serial.writeString("\n");
+				//serial.writeString("\n");
 				
 				//cout << "Written memblock and vblf \n";
 			
-				serial.writeString("\x1d\x2f\x0"); // print RAM image normal density pg171 of NCR7167
+				//serial.writeString("\x1d\x2f\x3"); // print RAM image normal density pg171 of NCR7167
 
 				//cout << "Done" << "\n";
 
@@ -58,7 +60,7 @@ serial.writeString("\x1b");
 			}
 		}
 		cout << "closing serial ";
-		serial.close();
+		//serial.close();
   
     } catch(boost::system::system_error& e)
     {
