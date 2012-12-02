37ac5802a21adc85da32aeada6622ed31fc13dc4
Max Revitt <max.revitt@gmail.com>
2012-12-02 17:47:53 +0000

    removed some graveyard code

--- c++/main.cpp
+++ c++/main.cpp
@@ -240,58 +240,5 @@ int main(int argc, char* argv[])
                 cout<<"Error: "<<e.what()<<endl;
                 return 1;
         }
-        
-        /*
-        ifstream::pos_type filesize;
-        char * memblock;
-        string stringblock;
-
-        try {
-            BufferedAsyncSerial serial("/dev/ttyUSB0",9600);
-                
-            ifstream inputfile ("smallface.bmp", ios::in|ios::binary|ios::ate);
-            if (inputfile.is_open())
-            {
-                filesize = inputfile.tellg();
-                memblock = new char [filesize];
-                inputfile.seekg (0, ios::beg);
-
-                //cout <<'\x1b';
-                serial.writeString("\x1b"); // BMP to RAM pg164 of NCR7167
-                stringblock = "";
-
-                stringblock+=("\x1b");
-
-                inputfile.read (memblock, filesize);
-                inputfile.close();
-
-                for (int i = 0; i < filesize; i++) // doesn't work with '\0' either
-                {
-                    stringblock+=(memblock[i]);
-                }
-
-                //serial.writeString("\x1b");
-
-                cout << memblock << endl;
-                cout << "Just about to write memblock " << filesize << endl;
-
-                serial.writeString(stringblock);
-
-                serial.writeString("\n");
-
-                cout << "Written memblock and vblf" << endl;
-
-                serial.writeString("\x1d\x2f\x0"); // print RAM image normal density pg171 of NCR7167
-
-                cout << "Done" << endl;
-
-                delete[] memblock;
-            }
-            cout << "closing serial" << endl;
-            serial.close();
-                } catch(boost::system::system_error& e)
-                {
-                    cout<<"Error: "<<e.what()<<endl;
-                    return 1;
-                }  */    
+          
 }
